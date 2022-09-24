6ba7c1bae14c126d9e508577d32e0f33ae1b6093
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-24 10:47:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('AzzBot-MD\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
-say(`'AzzBot' By YahyaGanzz || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
+say('SaxiaBot-MD\nWhatsApp Bot', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
+say(`'SaxiaBot' By Marcl || author}`, { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
 
 var isRunning = false
 /**
